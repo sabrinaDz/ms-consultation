7056e83363011077cc3b23cb7f9e789f3a3113b2
sabrinaDz <sabrinasahli@live.fr>
2021-09-10 01:35:19 -0700

    #fix. Add Repository annotation

--- src/main/java/com/sihatech/msconsultation/repository/ConsultationRepository.java
+++ src/main/java/com/sihatech/msconsultation/repository/ConsultationRepository.java
@@ -2,8 +2,10 @@ package com.sihatech.msconsultation.repository;
 
 import com.sihatech.msconsultation.data.Consultation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ConsultationRepository extends JpaRepository<Consultation, UUID> {
 }
