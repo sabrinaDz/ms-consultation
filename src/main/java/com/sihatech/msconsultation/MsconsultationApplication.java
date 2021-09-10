f910129b1cc9f5d707e35301fdae3675a5fa43ec
sabrinaDz <sabrinasahli@live.fr>
2021-09-10 01:30:56 -0700

    Consultation repository#3

---
+++ src/main/java/com/sihatech/msconsultation/MsconsultationApplication.java
@@ -0,0 +1,13 @@
+package com.sihatech.msconsultation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsconsultationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsconsultationApplication.class, args);
+    }
+
+}
